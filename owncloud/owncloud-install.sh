0e47ba20feb637c53b7e63e28ab92a4cbf2b10a9
jhegge <johnathan.hegge@nimbix.net>
2019-09-23 16:29:46 -0500

    re #1974
    usermod needs enhancing, not working

--- owncloud/owncloud-install.sh
+++ owncloud/owncloud-install.sh
@@ -226,7 +226,7 @@ fi
 
 # Done configuring, don't allow changes from the web interface
 #occ_cmd "config:system:set --type=bool --value=true config_is_read_only"
-usermod -G apache $OC_USER
+usermod -a -G apache $OC_USER
 chmod -R 0770 /var/www/html/owncloud/data
 chmod 777 /var/www/html/owncloud/lib/private/Config.php
 chmod 777 /var/www/html/owncloud/config/config.php
