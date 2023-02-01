65e527016dfe608860fef0cd55558d515ac4ce19
oxedions <oxedions@gmail.com>
2023-02-01 19:32:04 +0000

    JAR-6148

--- V2/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t jfm2:buff -f Dockerfile .
-docker run -d --name jfm2 jfm2:buff
-docker export jfm2 > flat.tar
-docker stop jfm2 && docker rm jfm2
-docker rmi jfm2:buff
-cat flat.tar | docker import - jfm2:latest
-
