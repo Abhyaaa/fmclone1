4cc6e60792da72b82062182dcbe1a30e83ebb162
Matthew Schramm <schrummy14@gmail.com>
2024-02-09 14:38:32 -0600

    Update to mirror current filemanager login

--- filebrowser/scripts/start.sh
+++ filebrowser/scripts/start.sh
@@ -42,18 +42,30 @@ if [[ -n $DELETE_OLD_DB ]]; then
     rm $DATABASE
 fi
 
+PATH=/opt/filebrowser:$PATH
+TOKEN=$(cat /etc/JARVICE/random128.txt | cut -c 1-64)
 if [[ ! -f $DATABASE ]]; then
     mkdir -p $(dirname $DATABASE)
     filebrowser -d $DATABASE config init > /dev/null 2> /dev/null
     filebrowser -d $DATABASE config set --commands $COMMANDS > /dev/null 2> /dev/null
-    filebrowser -d $DATABASE config set --auth.method noauth > /dev/null 2> /dev/null
+    filebrowser -d $DATABASE config set --auth.method nimbixpw > /dev/null 2> /dev/null
     filebrowser -d $DATABASE config set --perm.share=false > /dev/null 2> /dev/null
-    filebrowser -d $DATABASE users add $JARVICE_ID_USER nimbix > /dev/null 2> /dev/null
+    filebrowser -d $DATABASE users add $JARVICE_ID_USER $TOKEN > /dev/null 2> /dev/null
+else
+    filebrowser -d $DATABASE users update $JARVICE_ID_USER --password $TOKEN > /dev/null 2> /dev/null
+fi
+
+if [[ -n $DARK_MODE ]]; then
+    filebrowser -d $DATABASE config set --branding.color dark > /dev/null 2> /dev/null
+    filebrowser -d $DATABASE config set --branding.theme dark > /dev/null 2> /dev/null
 fi
 
 if [[ -n $JARVICE_INGRESSPATH ]]; then
-    BASEURL="/$JARVICE_INGRESSPATH/$(cat /etc/JARVICE/random128.txt | cut -c 1-64)"
+    BASEURL="/$JARVICE_INGRESSPATH"
 else
     BASEURL="/"
 fi
-filebrowser -d $DATABASE -r $STARTING_DIRECTORY --username $JARVICE_ID_USER --password nimbix --address 0.0.0.0 --port 5902 -b "$BASEURL"
+
+PORTNUM=${JARVICE_SERVICE_PORT:-5902}
+
+filebrowser -d $DATABASE -r $STARTING_DIRECTORY --address 0.0.0.0 --port "$PORTNUM" -b "$BASEURL"
